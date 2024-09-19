9bcc029a9cd9c99958f80be30fc72039bdbaffc0
andreySantoniello <andreynardy@gmail.com>
2024-09-19 18:07:16 -0300

    fix: go to top arrow and mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,37 @@
+const btnMenu = document.getElementById('btn-menu');
+const menu = document.getElementById('menu-mobile');
+const overlay = document.getElementById('overlay-menu');
 
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
-
-btnMenu.addEventListener('click', ()=>{
+btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu');
-})
+});
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu');
-})
 
-overlay.addEventListener('click', ()=>{
+const closeMenu = () => {
     menu.classList.remove('abrir-menu');
-})
+};
+
+menu.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+
+
+const scrollToSection = (event, sectionId) => {
+    event.preventDefault();
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+};
+
+const links = [
+    { id: 'inicio', section: 'inicioSecao' },
+    { id: 'especialidades', section: 'especialidadesSecao' },
+    { id: 'sobre', section: 'sobreSecao' },
+    { id: 'projetos', section: 'portifolioSecao' },
+    { id: 'contato', section: 'footerSecao' },
+    { id: 'seta', section: 'inicioSecao'}
+];
+
+links.forEach(link => {
+    document.getElementById(link.id).addEventListener('click', (event) => {
+        scrollToSection(event, link.section);
+    });
+});
